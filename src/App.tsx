2442992600aca1dba598db9e88cee27d3321e519
MuriloSouza955 <souza.murilo@gmail.com>
2025-07-23 14:38:11 -0300

    Hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,35 @@
-import "./global.css"
+import "./global.css";
+import { useEffect, useState } from "react";
 
 import { Button } from "./components/button";
-
-import styles from "./app.module.css"
+// import { useMessage } from "./hooks/useMessage"
+import styles from "./app.module.css";
 
 export function App() {
+  const [count, setCount] = useState(0);
+  // const message = useMessage({name: "Murilo", age: 18})
+
+  function handleAdd() {
+    setCount(count + 1);
+    
+  }
+
+  function handleRemove() {
+    if(count > 0){
+      setCount(count - 1);
+    }
+  }
+
+  useEffect(() => {
+    if (count > 0){
+      console.log( "O valor mudou para: " + count )
+    }
+  }, [count]);
   return (
     <div className={styles.container}>
-      <Button name="Adicionar"/>
-      <span>0</span>
-      <Button name="Remover"/>
+      <Button name="Adicionar" onClick={handleAdd} />
+      <span>{count}</span>
+      <Button name="Remover" onClick={handleRemove} />
     </div>
   );
 }
